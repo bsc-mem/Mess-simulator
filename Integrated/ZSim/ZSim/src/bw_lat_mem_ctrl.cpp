dcdec9964c1a7a8b393b2178cd60d8c1cd5ed471
Pouya Esmaili-Dokht <poyaesy@yahoo.com>
2024-08-14 15:46:13 +0330

    MessSimulator with ZSim for CXL memory expander

--- Integrated/ZSim/ZSim/src/bw_lat_mem_ctrl.cpp
+++ Integrated/ZSim/ZSim/src/bw_lat_mem_ctrl.cpp
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2024, Barcelona Supercomputing Center
+ * Contact: pouya.esmaili    [at] bsc [dot] es
+ *          petar.radojkovic [at] bsc [dot] es
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names
+ *       of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #include "bw_lat_mem_ctrl.h"
 
 BwLatMemCtrl::BwLatMemCtrl(string _curveAddress, uint32_t _curveWindowSize, double _MaxTheoreticalBW, uint32_t _domain, const g_string &_name) : domain(_domain)
@@ -50,12 +83,12 @@ BwLatMemCtrl::BwLatMemCtrl(string _curveAddress, uint32_t _curveWindowSize, doub
 			// the translation from Graviton3 to skylake frequency
 			//inputLatency = inputLatency * 2.1 / 2.6;
 			// the translation from fujitsu to skylake frequency
-			inputLatency = inputLatency * 2.1 / 2.2;
+			// inputLatency = inputLatency * 2.1 / 2.2;
 
 			// this is for skylake+DDR4
-   			//inputLatency = inputLatency - 51 - 4;
+   			inputLatency = inputLatency - 51 - 4;
    			//for graviton
-			inputLatency = inputLatency - 51 ;
+			// inputLatency = inputLatency - 51 ;
 			// this is for skylake+CXL (still we are 152 ns lead of. 36 ns more!)
 			// inputLatency = inputLatency;
 
